9e359bcca11ba1dca8735b6c935b22e1d225ae8a
subbylife <subbylife@gmail.com>
2025-01-26 17:11:57 +0900

    입력 이미지 및 예측 결과 디스플레이

--- predict.py
+++ predict.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
-from PIL import Image
 import torchvision.transforms as transforms
-import json
 import torch.nn.functional as F
 import os
 from models.convnet import ConvNet  # ConvNet 클래스 임포트
+import matplotlib.pyplot as plt
+from PIL import Image
+import platform # 폰트관련 운영체제 확인
 
 def predict_image(image_path, model_path, data_dir):
     # 장치 설정
@@ -28,7 +29,7 @@ def predict_image(image_path, model_path, data_dir):
     # 모델 초기화
     input_height = 28  # 입력 이미지 높이 # transform에서 적용한 사이즈
     input_width = 28   # 입력 이미지 너비 # transform에서 적용한 사이즈
-    model = ConvNet(num_classes, input_height, input_width).to(device)
+    model = ConvNet(device, num_classes, input_height, input_width).to(device)
     
     # 모델 가중치 로드
     model.load_state_dict(torch.load(model_path, weights_only=True))
@@ -48,16 +49,37 @@ def predict_image(image_path, model_path, data_dir):
     
     return predicted_class, probabilities, classes
 
+def image_show(image_path, predicted_class):
+    image = Image.open(image_path)
+    plt.imshow(image)
+    plt.axis('off')  # 축 숨기기
+    plt.title(f'입력이미지: {image_path} \n예측된 클래스: [{predicted_class}]')  # 예측된 클래스 제목 추가
+    plt.show()  # 이미지 출력
+
 if __name__ == "__main__":
+    # 한글 폰트 설정
+    if platform.system() == 'Windows':
+        font_name = 'Malgun Gothic'
+    elif platform.system() == 'Darwin':
+        font_name = 'AppleGothic'
+    else:
+        font_name = 'NanumGothic'
+
+    plt.rc('font', family=font_name)
+    plt.rcParams['axes.unicode_minus'] = False
+    
     # 예측 실행
-    image_path = "./data/dog_123.jpg"  # 예측하고 싶은 이미지 경로
-    # image_path = "./data/cat_123.jpg"  # 예측하고 싶은 이미지 경로
+    image_path = "./data/dog1.jpg"  # 예측하고 싶은 이미지 경로
+    # image_path = "./data/cat1.jpg"  # 예측하고 싶은 이미지 경로
     model_path = "trained_model.pth"         # 저장된 모델 경로
     # model_path = "best_model.pth"         # 저장된 모델 경로
     data_dir = "./data"                      # 데이터 디렉토리 경로
     
     predicted_class, probabilities, classes = predict_image(image_path, model_path, data_dir)
     
+    # 이미지 출력
+    image_show(image_path, predicted_class)
+
     print('클래스별 확률:')
     for cls, prob in zip(classes, probabilities):
         print(f'{cls}: {prob.item():.4f}')
